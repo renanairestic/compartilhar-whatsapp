068bb67ec03a753b7ad6323d7199e080d8101062
RenanAiresTIC <renan.aires.tic@gmail.com>
2021-03-26 13:15:08 -0300

    Up

--- App.js
+++ App.js
@@ -1,21 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, View, StyleSheet, Text, Linking, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const [whatsAppMessage, setWhatsAppMessage] = useState();
+
+  const sendMsg = () => {  
+   
+    let URL = 'whatsapp://send?text=' +  whatsAppMessage  ;
+
+    Linking.openURL(URL)
+      .then((data) => {
+        console.log('WhatsApp Aberto');
+      })
+      .catch(() => {
+        Alert.alert('WhatsApp não instalado');
+      });
+  };
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <View style={styleSheet.MainContainer}>
+
+        <Text style={styleSheet.text1}>Enviar mensagem para o WhatsApp app React Native</Text>
+
+        <TextInput
+          value={whatsAppMessage}
+          onChangeText={
+            (whatsAppMessage) => setWhatsAppMessage(whatsAppMessage)
+          }
+          placeholder={'Escreva aqui sua mensagem'}
+          style={styleSheet.textInputStyle}
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={styleSheet.button}
+          onPress={sendMsg}>
+          <Text style={styleSheet.buttonText}> Enviar mensagem para WhatsApp</Text>
+        </TouchableOpacity>
+      
     </View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const styleSheet = StyleSheet.create({
+ 
+  MainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FAFAFA',
+    padding: 10
   },
+
+  text1: {
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+
+  textInputStyle: {
+    height: 42,
+    borderColor: 'blue',
+    borderWidth: 1,
+    width: '100%',
+    paddingHorizontal: 10,
+    marginTop: 20
+  },
+
+  button: {
+    justifyContent: 'center',
+    marginTop: 18,
+    padding: 12,
+    backgroundColor: '#00B8D4',
+  },
+
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  
+ 
 });
---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Compartilhamento mensagem de Texto no WhatsApp
+Esse projeto faz parte de uma prática de estudos.
+
+Exemplo de compartilhamento de mensagem no WhatsApp.
+
+## Instruções:
+
+Foi utilizado bibliotecas no React Nativa, sem a necessidade de nenhum adicional. (linking)
+
+> expo init compartilhar-whatsapp
+
+Esse projeto foi adaptado a necessidade. 
+
+### Referências:
+* [CASTRO, Bruno. "Enviar mensagem para WhatsApp com React Nativa](https://brunolagoa.medium.com/enviar-mensagem-para-whatsapp-com-react-native-70239bb65495 'Tutorial')
+
+* [ReactNativeCode.com."Send WhatsApp Menssage From React Native App Exemple Tutorial"](https://reactnativecode.com/send-whatsapp-message-from-react-native/ 'ReactNativeCode.com')
